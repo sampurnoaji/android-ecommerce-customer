69e9d2c5aa7cc8128c8e24e11be3c32d9d8b47a4
Peter Sam <sam.krealogi@gmail.com>
2022-05-28 20:28:57 +0700

    add pref module

---
+++ app/src/main/java/id/petersam/android/starter/di/SharedPrefModule.kt
@@ -0,0 +1,27 @@
+package id.petersam.android.starter.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SharedPrefModule {
+
+    private const val PREF_NAME = "app_pref"
+
+    @Singleton
+    @Provides
+    fun providePref(@ApplicationContext context: Context): SharedPreferences =
+        context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+
+    @Singleton
+    @Provides
+    fun providePrefEditor(sharedPreferences: SharedPreferences): SharedPreferences.Editor =
+        sharedPreferences.edit()
+}
